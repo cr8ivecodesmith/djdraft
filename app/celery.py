4d28504f5edf51d45523a2f7efaa53cd4c1a6865
Matt Lebrun <matt@lebrun.org>
2016-10-03 18:29:57 +0800

    Updated app references

--- app/celery.py
+++ app/celery.py
@@ -5,7 +5,7 @@ import os
 from celery import Celery
 
 # set the default Django settings module for the 'celery' program.
-app_env = "can.settings.{}".format(
+app_env = "app.settings.{}".format(
     os.environ.get("APP_ENV", "prod")
 )
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", app_env)
